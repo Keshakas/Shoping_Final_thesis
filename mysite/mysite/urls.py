e85decdb5ccc91f6cdaa19763ac6ac0489c062a3
Keshakas <kestutis.kazakevicius@gmail.com>
2024-11-04 17:01:09 +0200

    Paleisti kiti html'ai per base.html

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include('shoping.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='', permanent=True)),
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
--- mysite/shoping/views.py
+++ mysite/shoping/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 def index(request):
     # return HttpResponse("Labas, pasauli!")
     return render(request, template_name="index.html")
+
+def resume(request):
+    return render(request, template_name="resume.html")
+
+def projects(request):
+    return render(request, template_name="projects.html")
+
+def contact(request):
+    return render(request, template_name="contact.html")
