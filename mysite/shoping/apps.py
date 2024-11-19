072c131d8fee4b546fdf192bea572ad717fb5237
Keshakas <kestutis.kazakevicius@gmail.com>
2024-11-19 13:42:42 +0200

    Sukurti signalai, automatiškai sukuriantys profilį sukūrus naują vartotoją.

--- mysite/shoping/apps.py
+++ mysite/shoping/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ShopingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shoping'
+
+    def ready(self):
+        from .signals import create_profile, save_profile
