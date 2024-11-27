b04c45b1836206b5c8637a2691c2bae255772862
Keshakas <kestutis.kazakevicius@gmail.com>
2024-11-27 17:16:07 +0200

    Smulkus pataisymai

--- mysite/shoping/admin.py
+++ mysite/shoping/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Store, Category, Product, ProductPrice, Profile, ShoppingCart, SavedResult
 
 
-class ProductPriceInline(admin.TabularInline):
-    model = ProductPrice
+class SavedResultInline(admin.TabularInline):
+    model = SavedResult
     extra = 0 # išjungia papildomas tuščias eilutes įvedimui
-    fields = ['product', 'price']
+    fields = ['name', 'store', 'price']
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -13,9 +13,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['name', 'category']
 
 
-class ProductPriceAdmin(admin.ModelAdmin):
-    list_display = ['product', 'store', 'price', 'cart']
-    list_filter = ['product', 'store', 'price']
+# class ProductPriceAdmin(admin.ModelAdmin):
+#     list_display = ['product', 'store', 'price', 'cart']
+#     list_filter = ['product', 'store', 'price']
 
 
 class SavedResultAdmin(admin.ModelAdmin):
@@ -25,13 +25,13 @@ class SavedResultAdmin(admin.ModelAdmin):
 
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ['date', 'user', 'total']
-    inlines = [ProductPriceInline]
+    inlines = [SavedResultInline]
     readonly_fields = ['date', 'total']
 
 admin.site.register(Store)
 admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductPrice, ProductPriceAdmin)
+# admin.site.register(ProductPrice, ProductPriceAdmin)
 admin.site.register(Profile)
 admin.site.register(ShoppingCart, ShoppingCartAdmin)
 admin.site.register(SavedResult, SavedResultAdmin)
